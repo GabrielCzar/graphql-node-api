fa8a39ce1c168f1a2b256e5b4f80793418b9f96e
Gabriel Cesar <gcal1@live.com>
2018-02-16 05:30:43 -0300

    confi express server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log('Hello, world!');
+import * as express from 'express';
+
+class App {
+    public express: express.Application;
+
+    constructor() {
+        this.express = express();
+        this.middleware();
+    }
+
+    private middleware(): void {
+        this.express.use('/hello', (req, res, next) => {
+            res.send({
+                hello: 'Hello world!'
+            })
+        });
+    }
+}
+
+export default new App().express;
