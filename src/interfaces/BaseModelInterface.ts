6acf7f66868c6dbab742e7e07114a24347b75fc7
Gabriel Cesar <gcal1@live.com>
2018-02-18 09:40:13 -0300

    config sequelize

--- src/interfaces/BaseModelInterface.ts
+++ src/interfaces/BaseModelInterface.ts
@@ -2,5 +2,5 @@ import { ModelsInterface } from "./ModelsInterface";
 
 export interface BaseModelInterface {
     prototype?;
-    associate?(models: ModelsInterface) : void;
+    associate?(models :ModelsInterface) :void;
 }
