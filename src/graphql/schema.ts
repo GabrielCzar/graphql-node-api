e83557202085fd5a57ce4cf5e669d1c02b66d3b3
Gabriel Cesar <gcal1@live.com>
2018-02-18 14:23:36 -0300

    modularizando schemas e types

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,51 +1,19 @@
 import { makeExecutableSchema } from 'graphql-tools'
 
-const users : any [] = [
-    {
-        id: 1,
-        name: 'John',
-        email: 'john@mail.com'
-    },
-    {
-        id: 2,
-        name: 'Fulano',
-        email: 'fulano@mail.com'
-    }
-]
+import { Mutation } from './mutation'
+import { Query } from './query'
 
-const typeDefs = `
-    type User {
-        id: ID!
-        name: String!
-        email: String!
-    }
+import { commentTypes } from './resources/comment/comment.schema'
+import { postTypes } from './resources/post/post.schema';
+import { userTypes } from './resources/user/user.schema';
 
-    type Query {
-        allUsers: [ User! ]!
-    }
-
-    type Mutation {
-        createUser(name: String!, email: String!): User
+const SchemaDefinition = `
+    type Schema {
+        query: Query
+        mutation: Mutation
     }
 `
 
-const resolvers = {
-    // implementando resolvers triviais
-    User: {
-        id: (parent) => parent.id,
-        name: (parent) => parent.name,
-        email: (parent) => parent.email
-    },
-    Query: {
-        allUsers: () => users
-    },
-    Mutation: {
-        createUser: (parent, params) => {
-            const newUser = Object.assign({ id: users.length + 1}, params)
-            users.push(newUser)
-            return newUser
-        }
-    }
-}
-
-export default makeExecutableSchema({ typeDefs, resolvers })
+export default makeExecutableSchema({ 
+    typeDefs: [ SchemaDefinition, Query, Mutation, commentTypes, postTypes, userTypes ]
+})
